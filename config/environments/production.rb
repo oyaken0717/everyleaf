faf6a026ddd18472a2ee662ea42af34e60696816
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-05-31 20:09:09 +0900

    [add] .envファイル設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  user_name: ENV['SENDGRID_USERNAME']
+  password: ENV['SENDGRID_PASSWORD']
 end
