b735d728bf4045443a2ace0bac56aa0a7620f222
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-06-11 18:20:39 +0900

    [add] RSpec新規、一覧、詳細作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,24 +1,74 @@
 require "rails_helper"
 
 describe "タスク管理機能", type: :system do
-  describe "一覧表示機能", type: :system do
+    # let(:user_a){ FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com") }
+    # let(:user_b){ FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com") }
+    let!(:task_a) { FactoryBot.create(:task, title: '最初のタスク', content: '最初のタスク', limit: '2019/06/01', status: "未着手") }#, user: user_a) 
+    
     before do
-      # user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@aexample.com')
-      FactoryBot.create(:task, title: '最初のタスク', content: '最初のタスク', limit: '2019/06/01', status: "未着手") #, user: user_a)
+      # visit login_path
+      # fill_in "メールアドレス", with: login_user.email
+      # fill_in "パスワード", with: login_user.password
+      # click_button "ログインする"
+      visit tasks_path
     end
 
+    shared_examples_for "ユーザーAが作成したタスクが表示される" do
+      it { expect(page).to have_content '最初のタスク' }
+    end
+  
+  describe "一覧表示機能" do
     context "ユーザーAがログインしているとき" do
-      # before do
-      #   visit login_path
-      #   fill_in "メールアドレス", with: "a@example.com"
-      #   fill_in "パスワード", with: "password"
-      #   click_button "ログインする"
-      # end
+      # let(:login_user) { user_a }
+      it_behaves_like "ユーザーAが作成したタスクが表示される"
+    end
 
-      it "ユーザーAが作成したタスクが表示される" do
-        visit tasks_path
-        expect(page).to have_content "最初のタスク"
+      # context "ユーザーBがログインしているとき" do
+        # let(:login_user){ user_b }
+
+        # it "ユーザーAが作成したタスクが表示されない" do
+        #   expect(page).to have_no_content "最初のタスク"
+        # end
+      # end
+  end
+
+  describe "詳細表示機能" do
+    context "ユーザーAがログインしているとき" do
+      # let(:login_user){ user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it_behaves_like "ユーザーAが作成したタスクが表示される"
+    end
+  end
+
+  describe "新規作成機能" do
+    # let(:login_user){ user_a }
+    before do
+      visit new_task_path
+      fill_in 'task_title', with: task_name
+      fill_in "task_content", with: "次のタスク"
+      fill_in "task_limit", with: Date.today
+      # fill_in "task_status", with: "未着手"
+      select "未着手", from: "task_status" 
+      click_button "登録する"
+    end
+
+    context "新規作成画面で名称を入力したとき" do
+      let(:task_name){ "次のタスク" }
+      it "正常に登録される" do
+        expect(page).to have_content "次のタスク"
       end
     end
-  end  
+
+    context "新規作成画面で名称を入力しなかったとき" do
+      let(:task_name){ "" }
+
+      it "エラーとなる" do
+        expect(page).to have_content "エラーがあります"
+      end
+    end
+  end
 end
