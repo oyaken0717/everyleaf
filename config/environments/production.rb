f5f48e7bf9eb490a8a30198b7dc5860fc1c338d9
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-06-17 19:21:07 +0900

    [add] Herokuデプロイ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  user_name: ENV['SENDGRID_USERNAME']
-  password: ENV['SENDGRID_PASSWORD']
+  # user_name: ENV['SENDGRID_USERNAME']
+  # password: ENV['SENDGRID_PASSWORD']
+  
 end
