d66b33ef6154e1a9e2506b9ce094f326062e539e
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-06-24 18:03:29 +0900

    [add] enum実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 1000 }
   validates :limit, presence: true
 
-  scope :search_title, -> (task_title) { where("title LIKE ?","%#{ task_title }%")}
-  scope :search_status, -> (task_status) { where(status: task_status)}
+  # scope :search_title, -> (task_title) { where("title LIKE ?","%#{ task_title }%")}
+  # scope :search_status, -> (task_status) { where(status: task_status)}
+
+  enum priority:{"低":0,"中":1,"高":2}
 end
